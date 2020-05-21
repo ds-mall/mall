8bf9d4578a9fb15df05ae1e71763b5f4c6b01426
dsying <dingduoduo0916@gmail.com>
2020-05-21 22:25:52 +0800

    聚合工程整合springboot

---
+++ mall-api/src/main/java/com/icoding/MallApplication.java
@@ -0,0 +1,11 @@
+package com.icoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MallApplication.class, args);
+  }
+}
