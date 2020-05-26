e504cd1d27311b3e20fb2454a776afbeaac055f9
dsying <dingduoduo0916@gmail.com>
2020-05-26 23:38:09 +0800

    Springboot 跨域解决方案: https://spring.io/guides/gs/rest-service-cors/

---
+++ mall-api/src/main/java/com/icoding/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.icoding.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Springboot 跨域解决方案
+ * 官方参考: https://spring.io/guides/gs/rest-service-cors/
+ */
+@Configuration
+public class CorsConfig {
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    WebMvcConfigurer webMvcConfigurer = new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                // 允许跨域的请求方式
+                .allowedMethods("*")
+                // 设置允许的header
+                .allowedHeaders("*")
+                // 是否发送cookie
+                .allowCredentials(true);
+      }
+    };
+    return webMvcConfigurer;
+  }
+}
+
